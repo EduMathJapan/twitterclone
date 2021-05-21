5327d907cada950d53ab57c5a55592dc560415b3
furu <education.in.sns@gmail.com>
2021-05-21 03:46:17 +0000

    test

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'MicropostsContorller@index') ;
+   
 Route::get('signup' , 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
 Route::post('signup','Auth\RegisterController@register')->name('signup.post');
 
@@ -24,3 +23,7 @@ Route::get('logout','Auth\LoginController@logout')->name('logout.get');
 Route::group(['middleware' => ['auth']], function () {
     Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
 });
+
+Route::group(['middlewate' => ['auth']],function(){
+    Route::resource('microposts','MicropostsController',['only'=>['store','destroy']]);
+}) ;
